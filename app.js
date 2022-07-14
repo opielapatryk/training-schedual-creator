43e568538cfede57c64238877eb4ca046c72dbf1
opielapatryk <xpatrykopiela@gmail.com>
2022-07-14 12:12:18 +0200

    master

---
+++ app.js
@@ -0,0 +1,9 @@
+const chesth1 = document.querySelector('.chest-h1')
+const chestUl = document.querySelector('.chest-ul')
+chesth1.addEventListener('click',()=>{
+    chestUl.style.display==='block' ? chestUl.style.display='none' : chestUl.style.display='block'
+})
+const atlas = {
+    header:"Chest",
+    
+}
